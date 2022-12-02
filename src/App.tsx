115e5b5b55c4532c118de5baee5c9e2e876a575c
Aleksvtop <vazilino@gmail.com>
2022-12-02 15:47:14 +0300

    Create on/off buttons

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import {OnOff} from './components/OnOff/OnOff'
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
+/*check commits*/
+
+function App() {
+    return (
+        /*<div>
+            <Title title={"Hello, my dear friend!"}/>
+            <Title title={"Something interesting"}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+            <Accordion titleValue={"Users"} collapsed={true}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
+        </div>*/
+        <>
+            <OnOff />
+            <OnOff />
+            <OnOff />
+        </>
+    );
+}
+/*type TitlePropsType = {
+    title: string;
+}
+function Title(props: TitlePropsType) {
+    console.log("Title rendering")
+    return (
+        <div>{props.title}</div>
+    )
+}*/
+
+
+export default App;
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+    collapsed: boolean
+}
+
+type AccordionTitlePropsType = {
+    title: string
+}
+
+
+export const Accordion = (props: AccordionPropsType) => {
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue}/>
+            {!props.collapsed && <AccordionBody/>}
+        </div>
+    )
+
+}
+
+const AccordionTitle = (props: AccordionTitlePropsType) => {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>{props.title}</h3>
+    )
+}
+
+
+const AccordionBody = () => {
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+export default Accordion;
