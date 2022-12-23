746abb5900c3b129e7833297a7ae2014443e069f
Aleksvtop <vazilino@gmail.com>
2022-12-24 00:16:40 +0300

    Create Uncontrolled star component

--- src/App.tsx
+++ src/App.tsx
@@ -14,18 +14,14 @@ function App() {
             <Title title={"Something interesting"}/>
             <Accordion titleValue={"Menu"} collapsed={false}/>
             <Accordion titleValue={"Users"} collapsed={true}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+
         </div>*/
         <div className={'App'}>
             <OnOff />
             <OnOff />
             <OnOff />
             <UncontrolledAccordion titleValue={'Menu'}/>
+            <Rating />
         </div>
     );
 }
--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -1,34 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 type RatingPropsType = {
-    value: 0 | 1 | 2 | 3 | 4 | 5
+    //value: 0 | 1 | 2 | 3 | 4 | 5
 }
 
 
 export function Rating(props: RatingPropsType) {
+    let [value, setValue] = useState(0)
+    const onClickHandler = () => {}
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
-
-
+            <Star selected={value > 0}/><button onClick={() => setValue(1)}>1</button>
+            <Star selected={value > 1}/><button onClick={() => setValue(2)}>2</button>
+            <Star selected={value > 2}/><button onClick={() => setValue(3)}>3</button>
+            <Star selected={value > 3}/><button onClick={() => setValue(4)}>4</button>
+            <Star selected={value > 4}/><button onClick={() => setValue(5)}>5</button>
         </div>
+
     )
 }
 
 function Star(props: any) {
-    console.log("Star rendering")
-    if (props.selected === true) {
-        return (
-            <span><b>*</b> </span>
-        )
-    } else {
-        return (
-            <span>*</span>
-        )
-    }
+    return (
+        props.selected ? <span><b>*</b> </span> : <span>*</span>
+    )
 
 }
